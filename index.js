1c8ab30c79b137ae30f85237bb51a510e21cb63c
krianlloydl <krianlloydl@gmail.com>
2024-12-28 16:33:27 +0800

    Added labels for tools images and wave animation

--- index.js
+++ index.js
@@ -6,3 +6,20 @@ document.querySelectorAll('#main-nav .nav-link').forEach(link => {
         this.classList.add('active');
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    VANTA.WAVES({
+        el: "#landing",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        color: 0x51414f,
+        shininess: 20.00,
+        waveHeight: 10.00,
+        waveSpeed: 0.80
+    })
+});
